9b7070e52031b700a3bfe804ce708b1c32e41646
Fernando Marino <fernandomarinosantos@gmail.com>
2026-06-29 15:39:52 -0700

    fix: changing .env variables to add _

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,12 +3,12 @@ import 'dotenv/config'
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(
-    process.env.MYSQLDATABASE,
-    process.env.MYSQLUSER,
-    process.env.MYSQLPASSWORD, 
+    process.env.MYSQL_DATABASE,
+    process.env.MYSQL_USER,
+    process.env.MYSQL_PASSWORD, 
     {
-        host: process.env.MYSQLHOST,
-        port: process.env.MYSQLPORT,
+        host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
         dialect: 'mariadb',
         logging: process.env.NODE_ENV === 'development' ? console.log : false,
     }
