fd50e2ea25da290a984224256f75a086cfa479e1
Fernando Marino <fernandomarinosantos@gmail.com>
2026-06-29 15:56:25 -0700

    fix: adjusting dialect to mariadb

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -5,7 +5,7 @@ import { Sequelize } from 'sequelize';
 console.log('Conectando ao banco de dados com as seguintes configurações:');
 console.log(`Host: ${process.env.MYSQL_URL}`);
 
-const sequelize = new Sequelize(process.env.MYSQL_URL, {
+const sequelize = new Sequelize(process.env.MYSQL_URL.replace('mysql://', 'mariadb://'), {
     dialect: 'mariadb',
     logging: process.env.NODE_ENV === 'development' ? console.log : false,
 });
