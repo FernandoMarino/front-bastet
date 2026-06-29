e25a102e1a5ba79db049b0b2caa78b1ed86699a6
Fernando Marino <fernandomarinosantos@gmail.com>
2026-06-29 15:53:00 -0700

    fix: changing DB connection to use Railway native env variables

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,24 +1,13 @@
-import 'dotenv/config'
+import 'dotenv/config';
 
-import { Sequelize } from "sequelize";
+import { Sequelize } from 'sequelize';
 
 console.log('Conectando ao banco de dados com as seguintes configurações:');
-console.log(`Host: ${process.env.MYSQL_HOST}`);
-console.log(`Port: ${process.env.MYSQL_PORT}`);
-console.log(`Database: ${process.env.MYSQL_DATABASE}`);
-console.log(`User: ${process.env.MYSQL_USER}`);
+console.log(`Host: ${process.env.MYSQL_URL}`);
 
-const sequelize = new Sequelize(
-    process.env.MYSQL_DATABASE,
-    process.env.MYSQL_USER,
-    process.env.MYSQL_PASSWORD, 
-    {
-        host: process.env.MYSQL_HOST,
-        port: process.env.MYSQL_PORT,
-        dialect: 'mariadb',
-        logging: process.env.NODE_ENV === 'development' ? console.log : false,
-    }
-
-)
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
+    dialect: 'mariadb',
+    logging: process.env.NODE_ENV === 'development' ? console.log : false,
+});
 
 export default sequelize;
