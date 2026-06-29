54da5f479e9af52e8f38809c9342a0d376c58d1c
Fernando Marino <fernandomarinosantos@gmail.com>
2026-06-29 14:15:57 -0700

    ajustando CORS para aceitar requisicões do frontend

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use(cookieParser())
 // Configurar o CORS para permitir requisições do frontend
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   }),
